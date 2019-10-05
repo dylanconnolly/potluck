95f9e3f5a8a18e7e1e195d19c29b1116c5b1f395
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-05 17:08:58 -0600

    Add dishes test to potluck test

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -16,4 +16,8 @@ class PotluckTest < Minitest::Test
   def test_it_has_a_date
     assert_equal "7-13-18", @potluck.date
   end
+
+  def test_it_has_an_empty_array
+    assert_equal [], @potluck.dishes
+  end
 end
